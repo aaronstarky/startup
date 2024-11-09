cfda52d6fe6bf9445192a3668a0b794fc2a3c6e1
aaronstarky <105680517+aaronstarky@users.noreply.github.com>
2024-11-08 17:45:07 -0700

    Setup page complete

--- my-react/src/setup.js
+++ my-react/src/setup.js
@@ -1,19 +1,22 @@
 export default function Setup() {
     return (
-        <div>
-            <h1>Setup</h1>
-            <div class="interactive-button">
-                <a class="header-link" href="track.html">BEGIN MATCH</a>
+        <div class="two-part-page">
+            <div class="page-info">
+                <h1>Setup</h1>
+                <div class="interactive-button">
+                    <a class="header-link" href="track.html">BEGIN MATCH</a>
+                </div>
+                <p>Invite friends in real-time to your game by searching for their usernames and clicking "Invite"</p>
             </div>
-            <p>Invite friends in real-time to your game by searching for their usernames and clicking "Invite"</p>
 
             <div class="player-search-box">
-                <h3>Placeholder for WebSocket Communication</h3>
-                <ul id="players">
-                </ul>
+                <h3>Database info placeholder</h3>
                 <div>
                     <label for="playerSearch">Player ID:</label>
                     <input type="text" name="playerSearch" id="playerSearch"/>
+                </div>
+                <div class="player-list">
+
                 </div>
             </div>
         </div>
